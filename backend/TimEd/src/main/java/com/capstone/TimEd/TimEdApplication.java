d6563127e6420b2f3f1aeb57c513d7d0d520e436
xxmu53xx <73223072+xxmu53xx@users.noreply.github.com>
2025-04-09 00:00:33 +0800

    added backend springboot, first commit

---
+++ backend/TimEd/src/main/java/com/capstone/TimEd/TimEdApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.TimEd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimEdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimEdApplication.class, args);
+	}
+
+}
