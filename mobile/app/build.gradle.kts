9381e02a78dc4cfe3d20fec233f1506aff7241e8
Mikhail James Navarro <mikhailjpn@gmail.com>
2025-04-11 22:15:55 +0800

    Adding Profile Page with attendance sheet Pop Up
    - Profile Page with the attendance sheet button functionality
    - Adding another for Profile Page AndroidManifest
    - adding another vector for profile
    - Few issue about the button color which is conflict during the res folder
    - April 11, 2025
    - 10:14 PM

--- mobile/app/build.gradle.kts
+++ mobile/app/build.gradle.kts
@@ -65,4 +65,6 @@ dependencies {
     androidTestImplementation ("androidx.test.ext:junit:1.1.5")
     androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("com.google.android.material:material:1.6.0")
+    //Second Insert Dependencies
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
 }
