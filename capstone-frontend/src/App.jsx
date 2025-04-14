f98f2fc1b0cb7d39207015208dc322f3c4c1b0d1
Clark Gemongala <clark.gemongala@cit.edu>
2025-04-14 21:54:53 +0800

    Adjusted Settings
    Fixed UI

--- capstone-frontend/src/App.jsx
+++ capstone-frontend/src/App.jsx
@@ -1,12 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './Dashboard/Dashboard';
 import Event from './Event/Event';
-<<<<<<< Updated upstream
 import LoginPage from './Auth/LoginPage';
-import Settings from './Settings/Settings';
-=======
 import Setting from './Setting/Setting';
->>>>>>> Stashed changes
 import './App.css';
 
 function App() {
@@ -17,11 +13,7 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/event" element={<Event />} />
-<<<<<<< Updated upstream
-        <Route path="/settings" element={<Settings />} />
-=======
         <Route path="/settings" element={<Setting />} />
->>>>>>> Stashed changes
       </Routes>
     </Router>
   );
